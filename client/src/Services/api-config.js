00a787614a2a9baf200877c0de8ee88b3117bd43
Daysired De Jesus <daisy@Daysireds-MacBook-Pro.local>
2021-06-12 10:41:38 -0400

    having deployment issues, making changes to some files to resolve issues

--- client/src/Services/api-config.js
+++ client/src/Services/api-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://piramide-guia-para-padres.herokuapp.com/' : 'http://localhost:3000' 
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://piramide-guia-para-padres.herokuapp.com/' : 'quirky-newton-e62d77.netlify.app' 
 
 const api = axios.create({
   baseURL: baseUrl
